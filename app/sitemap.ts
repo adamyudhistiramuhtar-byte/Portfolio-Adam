f27a74e4b2e9f2674c50f707684f8bf031c370ca
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-09-04 18:12:31 +0700

    Set sitemap to force static rendering

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next";
 
+// Tambahkan baris ini untuk memberitahu Next.js agar melakukan render statis
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
